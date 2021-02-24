4b6014cf30cf6968d06ba4adc6faca8755f6c4ad
malirezai <m.alirezaie@gmail.com>
2021-02-24 11:45:19 -0500

    adding appcenter post clone

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-wget https://github.com/github/codeql-action/releases/latest/download/codeql-runner-linux
-chmod +x codeql-runner-linux
+#codeql-runner-linux analyze --repository malirezai/android-codeql-sample
+#    --github-url https://github.com --github-auth $GITHUB_PAT_TOKEN
---
+++ app/appcenter-post-clone.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+wget https://github.com/github/codeql-action/releases/latest/download/codeql-runner-linux
+chmod +x codeql-runner-linux
+
+codeql-runner-linux init --repository malirezai/android-codeql-sample
+    --github-url https://github.com --github-auth $GITHUB_PAT_TOKEN
