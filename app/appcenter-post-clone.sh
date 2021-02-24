6b980c49df5bb9e26dd3a00ead6afbda65e509c9
malirezai <m.alirezaie@gmail.com>
2021-02-24 14:41:15 -0500

    scrupt changes

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -3,5 +3,7 @@
 wget https://github.com/github/codeql-action/releases/latest/download/codeql-runner-linux
 chmod +x codeql-runner-linux
 
+whereis codeql-runner-linux
+
 codeql-runner-linux init --repository malirezai/android-codeql-sample
     --github-url https://github.com --github-auth $GITHUB_PAT_TOKEN
